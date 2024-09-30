2ca05345cbecd8572f6bee0e7e2de7a6a1b08447
Manuel Fernández Crego <mfernandezc22@fpcoruna.afundacion.org>
2024-09-30 11:38:16 +0200

    Ejer13 Completado

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -1,14 +1,25 @@
 package com.campusdual.classroom;
 
 public class FuelTank {
+  public double actualFuel;
 
-  public int actualFuel = 10;
-
-  public void showDetails() {
-    System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
+  public FuelTank() {
+    this.actualFuel = 0;
   }
 
-  public int getActualFuel() {
-    return this.actualFuel;
+  public double getActualFuel() {
+    return actualFuel;
+  }
+
+  public void setActualFuel(double actualFuel) {
+    if (actualFuel >= 0) {
+      this.actualFuel = actualFuel;
+    } else {
+      System.out.println("No se puede asignar un valor negativo al combustible.");
+    }
+  }
+
+  public void showDetails() {
+    System.out.println("El nivel actual de combustible es: " + getActualFuel() + " litros.");
   }
 }
